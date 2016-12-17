1ddf1df7696dda1113dd070292615cfc6eb1c7ac
Wiktor Franus <killer982@o2.pl>
2016-12-17 17:52:03 +0100

    Add client views: make_order, search_item

---
+++ sklep/apps/client/urls.py
@@ -0,0 +1,8 @@
+from django.conf.urls import url
+from .views import *
+
+urlpatterns = [
+    url(r'^$', index),
+    url(r'^search/$', search_items, name='search'),
+    url(r'^order/$', make_order, name='order'),
+]
--- sklep/apps/client/views.py
+++ sklep/apps/client/views.py
@@ -1,3 +1,68 @@
 from django.shortcuts import render
+from apps.dao.models import *
+from apps.accounts.models import Client
+import json
+from datetime import datetime
+from django.db import transaction
+from django.http import HttpResponse
 
-# Create your views here.
+def index(request):
+	return render(request, 'index.html')
+
+def get_categories(request):
+	cats = Category.objects.all()
+	return cats
+
+def db_search_items(cat_id=None, name=None):
+	"""
+	Odwolaj sie do bd i pobierz wszystkie itemy z danej kategori i/lub majace w nazwie name
+	Jesli oba parametry sa None - zwraca wszystkie itemy
+	@return: lista Item
+	"""
+	items = Item.objects.all()
+	if cat_id is not None:
+		if name is None:
+			filtered_items = items.filter(category_id=cat_id)
+		else:
+			filtered_items = items.filter(category_id=cat_id, name__itcontains=name)
+		return filtered_items
+
+	return items
+
+def search_items(request, cat_id=None, name=None):
+	print(request.GET["cat_id"])
+	print(request.GET["name"])
+	return render(request, 'index.html')
+
+def make_order(request):
+	if request.method == 'POST':
+		#json_data = json.loads(request.body) #for python2?
+		order = json.loads(request.body.decode("utf-8"))
+		price_dict = {}
+		for itemspec in order['items']:
+			item_id = itemspec['id']
+			amount = itemspec['amount']
+			try:
+				item = Item.objects.get(id=item_id)
+				price_dict[item_id] = item.price
+				if item.in_stock < amount:
+					return HttpResponseBadRequest()
+			except Item.DoesNotExist:
+				return HttpResponseBadRequest()
+
+
+			with transaction.atomic():
+				new_order = Order(status_change_date=datetime.now,
+								payment_status=0,
+								client_id=Client.objects.get(user=request.user))
+				new_order.save()
+				for itemspec in order['items']:
+				# CHECK THIS: additional list of order_items may be required
+				# if objects is created locally and destroyed in the same iteration
+					order_item = Order_Item(quantity=itemspec['amount'],
+											price=price_dict[itemspec['id']],
+											order_id=new_order.id,
+											item_id=itemspec['id'])
+					order_item.save()
+
+	return HttpResponse()
