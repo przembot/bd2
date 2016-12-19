7b20f6451392f42684947bc45f9f0cda87d06ac6
quaster <quaster1303@gmail.com>
2016-12-19 23:55:01 +0100

    Link do opisu produktu

--- sklep/apps/client/urls.py
+++ sklep/apps/client/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     url(r'^$', index),
     url(r'^search/$', search_items, name='search'),
     url(r'^order/$', make_order, name='order'),
+    url(r'^product-details/$', product_detail, name='detail'),
 ]
--- sklep/apps/client/views.py
+++ sklep/apps/client/views.py
@@ -9,76 +9,85 @@ from django.contrib.auth.decorators import login_required
 
 
 def index(request):
-	return render(request, 'index.html')
+    return render(request, 'items.html')
 
 def get_categories(request):
-	cats = Category.objects.all()
-	return cats
+    cats = Category.objects.all()
+    return cats
+
+def product_detail(request, prod_id=None):
+    if prod_id is not None:
+        try:
+            product = Item.objects.get(id=prod_id)
+            return render(request, template_name='product-details.html', context={'product' : product})
+
+        except Item.DoesNotExist:
+            return render(request.path)
 
 def db_search_items(cat_id=None, name=None):
-	"""
-	Odwolaj sie do bd i pobierz wszystkie itemy z danej kategori i/lub majace w nazwie name
-	Jesli oba parametry sa None - zwraca wszystkie itemy
-	@return: lista Item
-	"""
-	items = Item.objects.all()
+    """
+    Odwolaj sie do bd i pobierz wszystkie itemy z danej kategori i/lub majace w nazwie name
+    Jesli oba parametry sa None - zwraca wszystkie itemy
+    @return: lista Item
+    """
+    items = Item.objects.all()
 
-	# return all items
-	if cat_id is None and name is None:
-		filtered_items = items
+    # return all items
+    if cat_id is None and name is None:
+        filtered_items = items
 
-	# search inside category
-	elif cat_id is not None:
-		if name is None:
-			filtered_items = items.filter(category_id=cat_id)
-		else:
-			filtered_items = items.filter(category_id=cat_id, name__itcontains=name)
+    # search inside category
+    elif cat_id is not None:
+        if name is None:
+            filtered_items = items.filter(category_id=cat_id)
+        else:
+            filtered_items = items.filter(category_id=cat_id, name__itcontains=name)
 
-	# search items by name without specifying category
-	else:
-		filtered_items = items.filter(name__itcontains=name)
+    # search items by name without specifying category
+    else:
+        filtered_items = items.filter(name__itcontains=name)
 
-	return filtered_items
+    return filtered_items
 
 def search_items(request, cat_id=None, name=None):
-	print(request.GET["cat_id"])
-	print(request.GET["name"])
-	return render(request, 'index.html')
+    print(request.GET["cat_id"])
+    print(request.GET["name"])
+    return render(request, 'index.html')
 
 @login_required
 def make_order(request):
-	if request.method == 'POST':
-		#json_data = json.loads(request.body) #for python2?
-		order = json.loads(request.body.decode("utf-8"))
-		price_dict = {}
-		for itemspec in order['items']:
-			item_id = itemspec['id']
-			amount = itemspec['amount']
-			try:
-				item = Item.objects.get(id=item_id)
-				price_dict[item_id] = item.price
-				if amount < '1' or item.in_stock < amount:
-					return HttpResponseBadRequest()
-			except Item.DoesNotExist:
-				return HttpResponseBadRequest()
+    if request.method == 'POST':
+        #json_data = json.loads(request.body) #for python2?
+        order = json.loads(request.body.decode("utf-8"))
+        price_dict = {}
+        for itemspec in order['items']:
+            item_id = itemspec['id']
+            amount = itemspec['amount']
+            try:
+                item = Item.objects.get(id=item_id)
+                price_dict[item_id] = item.price
+                if amount < '1' or item.in_stock < amount:
+                    return HttpResponseBadRequest()
+            except Item.DoesNotExist:
+                return HttpResponseBadRequest()
 
 
-			with transaction.atomic():
-				new_order = Order(status_change_date=datetime.now,
-								payment_status=0,
-								client_id=Client.objects.get(user=request.user))
-				new_order.save()
-				for itemspec in order['items']:
-				# CHECK THIS: additional list of order_items may be required
-				# if objects is created locally and destroyed in the same iteration
-					order_item = Order_Item(quantity=itemspec['amount'],
-											price=price_dict[itemspec['id']],
-											order_id=new_order.id,
-											item_id=itemspec['id'])
-					order_item.save()
+            with transaction.atomic():
+                new_order = Order(status_change_date=datetime.now,
+                                payment_status=0,
+                                client_id=Client.objects.get(user=request.user))
+                new_order.save()
+                for itemspec in order['items']:
+                # CHECK THIS: additional list of order_items may be required
+                # if objects is created locally and destroyed in the same iteration
+                    order_item = Order_Item(quantity=itemspec['amount'],
+                                            price=price_dict[itemspec['id']],
+                                            order_id=new_order.id,
+                                            item_id=itemspec['id'])
+                    order_item.save()
 
-				if order['invoice'] is True:
-					# create invoice here
-					pass
+                if order['invoice'] is True:
+                    # create invoice here
+                    pass
 
-	return HttpResponse()
+    return HttpResponse()
